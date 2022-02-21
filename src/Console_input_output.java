5ffe1680be0f15105acb6c3b4076dbaf53a5ff1e
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-02-21 00:17:53 -0600

    console input output

---
+++ src/Console_input_output.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+;
+public class Console_input_output {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter any integer");
+        int i = sc.nextInt();
+        System.out.println("the integer entered is  " + i);
+
+
+        File file = new File("input.txt");
+        try {
+            Scanner scan = new Scanner(file);
+            while (scan.hasNextInt()) {
+                System.out.println(scan.nextInt() * 100);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("error has occurred ");
+        }
+
+
+
+
+    }
+}
