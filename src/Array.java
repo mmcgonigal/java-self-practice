77517a1ded248f250f43a7c58d9143b4c89dce20
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-02-28 12:17:16 -0600

    practice on array

--- src/Array.java
+++ src/Array.java
@@ -1,13 +1,21 @@
 import java.util.Scanner;
 public class Array {
     // how to create array ::
-    //int [] nameOfArray = new int[ sizeOfArray" length " ex) 100 ] ;
-    public static int max(int a , int b){
-        return (a > b) ? a : b ;
+//    //int [] nameOfArray = new int[ sizeOfArray" length " ex) 100 ] ;
+//    public static int max(int a , int b){
+//        return (a > b) ? a : b ;
+//    }
+
+    //array
+    //int[] name = new int[100];  -->0(start )~99(end)  1 array with 100 index length.
+    public static int max (int a ,int b){
+        return (a>b) ? a : b;
     }
 
 
     public static void main(String[] args) {
+
+
 //        Scanner scanner  = new Scanner(System.in);
 //        System.out.print("Please type in the length of Array you are going to crete");
 //        int number = scanner.nextInt();
@@ -22,18 +30,43 @@ public class Array {
 //        }
 //        System.out.println("the largest number is " + result + " .");
 
-        int [] newArray = new int[100];
-        for(int i = 0; i < 100; i++){
-            newArray[i] = (int)(Math.random() * 100 + 1) ;
-            // Math.random() creates random floats of 0.xx ~ 1  0 <= x < 1
-            // therefore, multiply 100 to make it 1 <= x <100
-            // then plus 1 ---> 1 <= x < 101
+//        int [] newArray = new int[100];
+//        for(int i = 0; i < 100; i++){
+//            newArray[i] = (int)(Math.random() * 100 + 1) ;
+//            // Math.random() creates random floats of 0.xx ~ 1  0 <= x < 1
+//            // therefore, multiply 100 to make it 1 <= x <100
+//            // then plus 1 ---> 1 <= x < 101
+//        }
+//        int sum = 0;
+//        for( int i = 0; i < 100 ; i ++){
+//            sum += newArray[i];
+//        }
+//        System.out.println("100s of random number's average is " + sum /100);
+
+
+        Scanner scan =  new Scanner(System.in);
+        System.out.println("please set a size of array ! ");
+        int number = scan.nextInt();
+        int[] array  = new int[number];
+        for (int i = 0; i <number ; i++){
+            System.out.print("please input integer for each index.");
+            array[i] = scan.nextInt();
         }
-        int sum = 0;
-        for( int i = 0; i < 100 ; i ++){
-            sum += newArray[i];
+        int result = -1;
+        for (int i = 0; i< number ; i ++){
+            result = max(result,array[i]);
         }
-        System.out.println("100s of random number's average is " + sum /100);
+        System.out.println("the largest number is " + result + " .");
+//
+//        int[] arr = new int [100];
+//        for( int i = 0; i < number ; i ++){
+//            arr[i] = (int)(Math.random() *100 +1);
+//        }
+//
+
+
+
+
 
 
     }
