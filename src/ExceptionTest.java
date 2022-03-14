fc2b9af9080025d7fb5775221f142115305ddb65
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-03-13 23:18:48 -0500

    practice on try- catch

--- src/ExceptionTest.java
+++ src/ExceptionTest.java
@@ -48,6 +48,21 @@ public class ExceptionTest {
         System.out.println(7);
         System.out.println(8);
 
+        System.out.println(1);
+        System.out.println(2);
+
+        try{
+            System.out.println(3);
+            System.out.println(0/0);
+            System.out.println((4));
+        }catch(ArithmeticException ae){
+            ae.printStackTrace();
+            System.out.println("ae.getMessage() = " + ae.getMessage());
+        }
+        System.out.println(6);
+
+
+
 
 
 //
