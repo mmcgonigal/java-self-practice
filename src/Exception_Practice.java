83b2d3b0a1b948759054c157c6a6bd63eb0f4035
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-03-14 23:45:36 -0500

    exception - throws/ throw difference practice

---
+++ src/Exception_Practice.java
@@ -0,0 +1,22 @@
+import java.io.File;
+
+public class Exception_Practice {
+
+    static File createFile(String filename) throws Exception{
+        if (filename == null || filename.equals(""))
+            throw new Exception("not valid name of the file");
+        File f = new File(filename);
+        f.createNewFile();
+        return f;
+    }
+    public static void main(String[] args) {
+        try {
+            File f = createFile(args[0]);
+            System.out.println(f.getName() + " successfully created a file");
+        }catch(Exception e){
+            System.out.println(e.getMessage() + " Please re-enter the name of file");
+        }
+    }
+
+
+}
