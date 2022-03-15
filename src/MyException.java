8c6e4f7dfe4ec1b62d4d6dd463d473a529aeabf8
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-03-15 00:23:47 -0500

    custom Exception

---
+++ src/MyException.java
@@ -0,0 +1,15 @@
+public class MyException extends Exception{
+    private final int ERR_CODE;
+
+     MyException(String msg, int err_code) {
+        super(msg);
+        ERR_CODE = err_code;
+    }
+    MyException(String msg){
+         this(msg, 100);
+    }
+    public int getERR_CODE(){
+         return ERR_CODE;
+    }
+
+}
