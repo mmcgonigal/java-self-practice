0643cc55563df4bed99ea6aec5e6e3d5d05dd11e
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-03-20 22:56:17 -0500

    Equals

---
+++ src/Equals.java
@@ -0,0 +1,14 @@
+public class Equals {
+//    hashcode()   --> return hashcode of the instances.
+
+    public static void main(String[] args) {
+        String str1 = new String ("abc");
+        String str2 = new String("abc");
+        System.out.println("str1.equals(str2) = " + str1.equals(str2));
+        System.out.println("str1.hashCode() = " + str1.hashCode());
+        System.out.println("str2.hashCode() = " + str2.hashCode());
+
+
+
+    }
+}
