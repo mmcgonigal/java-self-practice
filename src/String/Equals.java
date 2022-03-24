fa6d4299c81a9134e1d29c143929e9ea6b836536
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-03-23 21:17:35 -0500

    equal to  /assign to

--- src/String/Equals.java
+++ src/String/Equals.java
@@ -1,5 +1,5 @@
 package String;
-
+import java.util.ArrayList;
 public class Equals {
 //    hashcode()   --> return hashcode of the instances.
 
@@ -12,5 +12,43 @@ public class Equals {
 
 
 
+        ArrayList<String> favFoods = new ArrayList<>();
+        favFoods.add("sushi");
+        favFoods.add("enchiladas verdes");
+        favFoods.add("chicken tikka masala");
+
+        System.out.println("favfoods = " + favFoods);
+
+        ArrayList<String> favFoods2 = new ArrayList<>();
+
+        favFoods2 = favFoods; //Copied the whole object + ref over
+
+        System.out.println("favFoods2.hashCode() = " + favFoods2.hashCode());
+        System.out.println("favfoods.hashCode() = " + favFoods.hashCode());
+
+        System.out.println("favFoods == favFoods2 = " + (favFoods == favFoods2));
+
+
+        favFoods2 = new ArrayList<>();
+        for (String food : favFoods){
+            favFoods2.add(food);
+        }
+        System.out.println("(favFoods == favFoods2) = " + (favFoods == favFoods2));
+
+        for(String food : favFoods){
+            if(food.equalsIgnoreCase( "enchiladas verdes")){
+                System.out.println(food + "? I love some mexican food");
+            } else {
+                System.out.println(food + " is pretty good too");
+            }
+        }
+
     }
+
+
+
+
+
+
+
 }
