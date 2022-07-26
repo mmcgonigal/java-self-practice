e2e59e08856f08e56d3143ac4277df8cf861234e
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-07-25 23:24:23 -0500

    double conditional - if inside if

--- src/Conditional/Conditional.java
+++ src/Conditional/Conditional.java
@@ -23,6 +23,33 @@ public class Conditional {
         System.out.println("Your grade is " + grade + " .");
 
 
+        char 학점 = 'C',opt = '0';
+        System.out.println("Please enter your score") ;
+        Scanner scan = new Scanner(System.in);
+        int point =scan.nextInt();
+        System.out.printf("Your score is %d.%n",point );
+        if(point >=90){
+            학점 = 'A';
+            if (point >= 98) {
+                opt = '+';
+            } else if (point < 94) {
+                opt = '-';
+            }
+        } else if (학점 >= 80) {
+            grade = 'B';
+            if (학점 >= 88) {
+                opt = '+';
+            }else if (학점 < 84){
+                opt = '-';
+            }
+        }
+
+        }
+
+
+
+
+
     }
 }
 
