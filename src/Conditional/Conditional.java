2b93e717fe929e0c2882a3e2e8a4cbcf1de61578
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-07-26 23:28:56 -0500

    condition switch

--- src/Conditional/Conditional.java
+++ src/Conditional/Conditional.java
@@ -6,15 +6,15 @@ public class Conditional {
 
     public static void main(String[] args) {
         int score = 0;
-        char grade =' ';
+        char grade = ' ';
         System.out.print("please input the score");
         Scanner scanner = new Scanner(System.in);
         score = scanner.nextInt();
 
-        if (score >=90 ){
-            grade ='A';
-        }else if (score >=80){
-            grade='B';
+        if (score >= 90) {
+            grade = 'A';
+        } else if (score >= 80) {
+            grade = 'B';
         } else if (score >= 70) {
             grade = 'C';
         } else {
@@ -23,12 +23,12 @@ public class Conditional {
         System.out.println("Your grade is " + grade + " .");
 
 
-        char 학점 = 'C',opt = '0';
-        System.out.println("Please enter your score") ;
+        char 학점 = 'C', opt = '0';
+        System.out.println("Please enter your score");
         Scanner scan = new Scanner(System.in);
-        int point =scan.nextInt();
-        System.out.printf("Your score is %d.%n",point );
-        if(point >=90){
+        int point = scan.nextInt();
+        System.out.printf("Your score is %d.%n", point);
+        if (point >= 90) {
             학점 = 'A';
             if (point >= 98) {
                 opt = '+';
@@ -39,10 +39,33 @@ public class Conditional {
             grade = 'B';
             if (학점 >= 88) {
                 opt = '+';
-            }else if (학점 < 84){
+            } else if (학점 < 84) {
                 opt = '-';
             }
         }
+
+
+        System.out.println("what month is it?");
+        int month = scan.nextInt();
+        switch (month) {
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("It is spring");
+                break;
+            case 6: case 7: case 8:
+                System.out.println("It is summer");
+                break;
+            case 9: case 10: case 11:
+                System.out.println("It is fall");
+//            case 12 : case 1 : case 2:
+//                System.out.println("It is winter");
+//                break;
+            default:
+                System.out.println("it is winter");
+        }
+
+
 
         }
 
@@ -51,5 +74,5 @@ public class Conditional {
 
 
     }
-}
+
 
